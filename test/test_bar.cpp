f750b4e0fbd30679d1dc9fc8264205108ed93f78
frankfu <ffu@keywcorp.com>
2021-01-15 23:49:41 -0500

    Show option of building a single Boost Test module over multiple cpp files.

--- test/test_bar.cpp
+++ test/test_bar.cpp
@@ -1,9 +1,17 @@
+// BOOST_TEST_DYN_LINK can be a target compile definition
+// #define BOOST_TEST_DYN_LINK
 #include <boost/test/unit_test.hpp>
-
 #include "Bar.h"
+
+// Test suite needed if building one test module over multiple cpp files.
+// Not needed if using ctest and building separate modules per cpp file.
+
+BOOST_AUTO_TEST_SUITE(testsuitebar)
 
 BOOST_AUTO_TEST_CASE(constructor_test) {
     Bar b;
     BOOST_CHECK_EQUAL(b.x(), 42);
     BOOST_CHECK_EQUAL(b.y(), 43);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
--- test/test_foo.cpp
+++ test/test_foo.cpp
@@ -1,6 +1,13 @@
-#include <boost/test/unit_test.hpp>
+// BOOST_TEST_DYN_LINK set as a target compile definition
+// #define BOOST_TEST_DYN_LINK
 
+#include <boost/test/unit_test.hpp>
 #include "Foo.h"
+
+// Test suite needed if building one test module over multiple cpp files.
+// Not needed if using ctest and building separate modules per cpp file.
+
+BOOST_AUTO_TEST_SUITE(testsuitefoo)
 
 BOOST_AUTO_TEST_CASE(constructor_test) {
     Foo f;
@@ -9,3 +16,5 @@ BOOST_AUTO_TEST_CASE(constructor_test) {
     Foo f2{24};
     BOOST_REQUIRE_EQUAL(f2.x(), 24);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
